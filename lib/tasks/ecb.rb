cd498a7129859c31878d58857062a1263e836d71
Dariusz Albrecht <dariusz.albrecht@gmail.com>
2017-11-18 17:58:07 +0100

    Add rake task for easier exchange rates updating

---
+++ lib/tasks/ecb.rb
@@ -0,0 +1,13 @@
+namespace :ecb do
+  desc "Fetch rates from European Central Bank and append any missing records to database"
+  task import_rates: :environment do
+    import_result = ImportRatesFromEcb.new.call
+
+    if import_result.success?
+      puts import_result.resolve_message
+    else
+      "There were some errors while trying to import new rates:\n" \
+      "#{import_result.error_message}"
+    end
+  end
+end
